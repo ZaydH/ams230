cd0c9e03009336a6ea693f46672993cb801b73fa
Zayd Hammoudeh <zhammoud@ucsc.edu>
2018-05-20 03:54:14 -0700

    Integrating line search into lbfgs and more of the basic structure of the program

--- hw/hw04/src/lbfgs.py
+++ hw/hw04/src/lbfgs.py
@@ -4,22 +4,34 @@ import numpy as np
 
 class LBFGS:
     STOP_ERR = 10 ** -14
+    MAX_LINE_SEARCH_ATTEMPTS = 20
 
     def __init__(self, n: int, alpha: float):
         """
         Constructor for the Limited Memory BFGS function.
 
-        :param n: Dimension of the function
+        :param n: Dimension of the function. (Must be even)
         :param alpha:
         """
+        assert n > 0 and n % 2 == 0
         self._n = n
         self._alpha = alpha
         self.m = None
 
         self._x0 = -1 * np.ones(n)
         self._x_opt = np.ones(n)
+        self._x = None
+
+        self._H = None  # type: np.ndarray
+        self._p = None
+
+        # Values for line search
+        self._alpha0 = 1
+        self._c1 = 0.1
+        self._c2 = 0.45
 
         self._k = None
+        self._gamma = None
         self._S = None
         self._Y = None
 
@@ -27,25 +39,43 @@ class LBFGS:
 
     def run(self):
         """
-        Based on
+        Based on Algorithm 7.5 (L-BFGS) of Nocedal and Wright (pg. 179).
         :return:
         """
+        assert self.m is not None and self.m > 0
+
         self._k = 0
-        x_k = self._x0
+        self._x = self._x0
 
         self.err = []
         self._S = []
         self._Y = []
         while True:
-            self.err.append(self._f(x_k))
+            self.err.append(self._f(self._x))
             if self.err[-1] < LBFGS.STOP_ERR:
                 return
 
+            # Limit the recursion depth
+            if self._k > self.m:
+                self._S = self._S[1:]
+                self._Y = self._Y[1:]
+            # Calculate gamma as defined in Eq. (7.20) of Nocedal and Wright
+            if self._k == 0:
+                # ToDo Confirm gamma_0 value
+                self._gamma = 1
+            else:
+                self._gamma = (self._S[-1].T @ self._Y[-1]) / (self._Y[-1].T @ self._Y[-1])
+
+            d_0 = np.zeros((self._n, self._n))  # ToDo Verify the value of d0
+            self._H = self._bfgs_rec(d_0, min(self._k, self.m))
+            self._p = -1 * self._H @ self._g(self._x)
+            alpha_k = self._line_search()
+
             # Update the stored memory parameters
-            x_prev = x_k
-            x_k = None
-            self._S.append(x_k - x_prev)
-            self._Y.append(self._g(x_k) - self._g(x_prev))
+            x_prev = self._x
+            self._x += alpha_k * self._p
+            self._S.append(self._x - x_prev)
+            self._Y.append(self._g(self._x) - self._g(x_prev))
             self._k += 1
 
     def _f(self, x: np.ndarray) -> float:
@@ -61,6 +91,17 @@ class LBFGS:
             i = 2 * j - 1
             f_x += self._alpha * (x[i] - (x[i - 1] ** 2)) ** 2 + (1 - x[i - 1]) ** 2
         return f_x
+
+    def _phi(self, alpha: float) -> float:
+        return self._f(self._x + alpha * self._p)
+
+    def _d_phi(self, alpha):
+        """
+        Derivative of $\phi(x+\alpha p)$ for use in line search.
+
+        :return:
+        """
+        return self._g(self._x + alpha * self._p).T @ self._p
 
     def _g(self, x) -> np.ndarray:
         """
@@ -80,25 +121,84 @@ class LBFGS:
         grad_np = np.array(grad)
         return grad_np
 
-    def _bfgs_rec(self, d, gamma, k):
+    def _bfgs_rec(self, d: np.ndarray, k: int) -> np.ndarray:
         """
         Recursive implementation of BFGS.
 
         :param d:
-        :param gamma:
         :param k: Remaining depth of the recursion.
         :return:
         """
         if k == 0:
-            return gamma * np.identity(self._n)
+            return self._gamma * np.identity(self._n)
         else:
-            s_k = self._S[k]
-            y_k = self._Y[k]
+            # Subtract one since indexed from 0
+            s_k = self._S[k - 1]
+            y_k = self._Y[k - 1]
 
             alpha = (s_k.T @ d) / (y_k.T @ s_k)
             d = d - alpha * y_k
-            d = self._bfgs_rec(d, gamma, k - 1)
+            d = self._bfgs_rec(d, k - 1)
             return d + (alpha - (y_k.T @ d) / (y_k.T @ s_k)) * s_k
+
+    def _line_search(self) -> float:
+        """
+        Calculates alpha for line search.
+
+        :return: alpha for the function.
+        """
+        prev_alpha = self._alpha0
+        alpha = self._alpha0 + 1
+        phi_alpha = self._phi(self._alpha0)  # This is overwritten inside the loop
+
+        phi_al_0 = self._phi(self._alpha0)
+        d_phi_al_0 = self._d_phi(self._alpha0)
+        for i in range(1, LBFGS.MAX_LINE_SEARCH_ATTEMPTS + 1):
+            prev_phi_alpha = phi_alpha
+
+            phi_alpha = self._phi(alpha)
+            if phi_alpha > phi_al_0 + self._c1 * alpha * d_phi_al_0 \
+                    or (i > 1 and phi_alpha >= prev_phi_alpha):
+                return self.zoom(prev_alpha, alpha)
+
+            d_phi_alpha = self._d_phi(alpha)
+            if abs(d_phi_alpha) <= -self._c2 * d_phi_al_0:
+                return alpha
+            if d_phi_alpha >= 0:
+                return self.zoom(alpha, prev_alpha)
+
+            prev_alpha = alpha
+            alpha *= 2
+        assert False
+
+    def zoom(self, alpha_lo: float, alpha_hi: float) -> float:
+        """
+        Perform Binary search within the range above to find the best value.
+
+        :param alpha_lo:
+        :param alpha_hi:
+        :return: Optimal alpha value for line search.
+        """
+        phi_al_0 = self._phi(self._alpha0)
+        d_phi_al_0 = self._d_phi(self._alpha0)
+        while True:
+            alpha_m = (alpha_lo + alpha_hi) / 2
+            # Handle the case where floating point error is too high
+            if alpha_hi == alpha_lo:
+                return alpha_m
+
+            phi_alpha = self._phi(alpha_m)
+
+            if phi_alpha > phi_al_0 + self._c1 * alpha_m * d_phi_al_0 \
+                    or phi_alpha >= self._phi(alpha_lo):
+                alpha_hi = alpha_m
+            else:
+                d_phi_alpha = self._d_phi(alpha_m)
+                if abs(d_phi_alpha) <= -self._c2 * d_phi_al_0:
+                    return alpha_m
+                if d_phi_alpha * (alpha_hi - alpha_lo) >= 0:
+                    alpha_hi = alpha_lo
+                alpha_lo = alpha_m
 
 
 if __name__ == "__main__":
@@ -113,5 +213,5 @@ if __name__ == "__main__":
         file_name = f"lbfgs_m={_m}.csv"
         with open(file_name, "w") as fout:
             fout.write("k,f_err")
-            for k, err in enumerate(lbfgs.err):
-                fout.write(f"\n{k},{err}")
+            for _k, err in enumerate(lbfgs.err):
+                fout.write(f"\n{_k},{err}")
